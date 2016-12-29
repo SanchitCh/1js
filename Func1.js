f80550143a4919bd23ac918dd7d5722fd10aff60
SanchitCh <choudhary.sanchit855@gmail.com>
2016-12-29 12:38:49 +0530

    Add files via upload

---
+++ Func1.js
@@ -0,0 +1,11 @@
+var a=2;
+console.log(a);
+
+function name() {
+  console.log(a);
+}
+name();
+console.log(a);
+
+/* var a is accessible inside as well as outside the function. As it is globally accessible,
+ Hence it is a global scope.*/
---
+++ Func2.js
@@ -0,0 +1,8 @@
+function name() {
+  var a=2;
+  console.log(a);
+}
+console.log(a);
+
+// var a is accessible only inside the function and cannot be accessed outside the function.
+// Hence it is a local scope.

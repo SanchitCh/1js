8db179a25dae9353eaeeab51215d235a91cb2185
SanchitCh <choudhary.sanchit855@gmail.com>
2016-12-29 12:06:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+alert('WELCOME TO  MEAN STACK COURSE');
+var a =5;
+console.log(a);
+
+function add() {
+  var b=6;
+  console.log(a);
+  console.error('ERROR');
+  console.log(b);
+}
+add();
+console.log(a);
+console.log(b);
